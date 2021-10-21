175a54647ab47ff24bff2dfe17020a747bf51612
shamapumbu <88844339+shamapumbu@users.noreply.github.com>
2021-10-21 08:28:25 +0200

    Search files
    Search Files added

---
+++ search/index.php
@@ -0,0 +1,104 @@
+<?php
+include 'connect_test_db.php';
+$searchErr = '';
+$employee_details='';
+if(isset($_POST['save']))
+{
+    if(!empty($_POST['search']))
+    {
+        $search = $_POST['search'];
+        $stmt = $con->prepare("select * from employee_info where department like '%$search%' or name like '%$search%'");
+        $stmt->execute();
+        $employee_details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //print_r($employee_details);
+         
+    }
+    else
+    {
+        $searchErr = "Please enter the information";
+    }
+    
+}
+ 
+?>
+<html>
+<head>
+<title>ajax example</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="bootstrap-theme.css" crossorigin="anonymous">
+<style>
+.container{
+    width:70%;
+    height:30%;
+    padding:20px;
+}
+</style>
+</head>
+ 
+<body>
+    <div class="container">
+    <h3><u>PHP MySQL search database and display results</u></h3>
+    <br/><br/>
+    <form class="form-horizontal" action="#" method="post">
+    <div class="row">
+        <div class="form-group">
+            <label class="control-label col-sm-4" for="email"><b>Search Employee Information:</b>:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" name="search" placeholder="search here">
+            </div>
+            <div class="col-sm-2">
+              <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
+            </div>
+        </div>
+        <div class="form-group">
+            <span class="error" style="color:red;">* <?php echo $searchErr;?></span>
+        </div>
+         
+    </div>
+    </form>
+    <br/><br/>
+    <h3><u>Search Result</u></h3><br/>
+    <div class="table-responsive">          
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Employee Name</th>
+            <th>Phone No</th>
+            <th>Age</th>
+            <th>Department</th>
+          </tr>
+        </thead>
+        <tbody>
+                <?php
+                 if(!$employee_details)
+                 {
+                    echo '<tr>No data found</tr>';
+                 }
+                 else{
+                    foreach($employee_details as $key=>$value)
+                    {
+                        ?>
+                    <tr>
+                        <td><?php echo $key+1;?></td>
+                        <td><?php echo $value['name'];?></td>
+                        <td><?php echo $value['phone_no'];?></td>
+                        <td><?php echo $value['age'];?></td>
+                        <td><?php echo $value['department'];?></td>
+                    </tr>
+                         
+                        <?php
+                    }
+                     
+                 }
+                ?>
+             
+         </tbody>
+      </table>
+    </div>
+</div>
+<script src="jquery-3.2.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
